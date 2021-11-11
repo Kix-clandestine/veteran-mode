6a6450dd10680a74b60ca96519533b90563160db
David Sanders <david.wm.sanders@gmail.com>
2021-11-11 23:03:21 +0000

    Add script to start rwr_server for veteran-mode

---
+++ py/start_vm_invasion_server.py
@@ -0,0 +1,82 @@
+import itertools
+import pathlib
+import subprocess
+
+# this magic allows for Ctrl+C to PyCharm run console to be handled nicely
+try:
+    from console_thrift import KeyboardInterruptException as KeyboardInterrupt  # noqa
+except ImportError:
+    pass
+
+SCRIPT_DIR = pathlib.Path(__file__).parent
+AS_SCRIPT = SCRIPT_DIR / "start_vm_invasion_server.as"
+RWR_ROOT = pathlib.Path(r"C:\Program Files (x86)\Steam\steamapps\common\RunningWithRifles")
+RWR_SERV = RWR_ROOT / "rwr_server.exe"
+PKGS_ROOT = RWR_ROOT / "media/packages"
+TEST_PKG_DIR = PKGS_ROOT / "veteran-mode"
+
+
+def _consume_prompt(proc):
+    # consume the prompt marker
+    prompt_marker = proc.stdout.read(1)
+    if prompt_marker != ">":
+        raise Exception(f"prompt marker read got unexpected '{prompt_marker}' :/")
+
+
+def wait_for_server_load(proc):
+    _spinner_steps = itertools.cycle(["/", "-", "\\", "|"])
+    while True:
+        # read a line from rwr server stdout
+        _output_line = proc.stdout.readline().lstrip(">")
+        # strip the line for easier processing
+        _stripped_line = _output_line.strip()
+        # print(f"{stripped_line=!r}")
+        if _stripped_line.startswith("Loading"):
+            _s = next(_spinner_steps)
+            print(f"{_s} Loading...", end="\r")
+        elif _stripped_line == "Game loaded":
+            # exit the while loop now
+            break
+        else:
+            print(_stripped_line)
+    return True
+
+
+def send_command(proc, cmd: str):
+    proc.stdin.write(f"{cmd}\n")
+    proc.stdin.flush()
+
+
+if __name__ == '__main__':
+    # start an rwr server
+    path_to_package = f"media/packages/{TEST_PKG_DIR.name}"
+    print(f"Starting RWR server for '{path_to_package}' package...")
+    rwr_serv_args = [f"{RWR_SERV}"]
+    # rwr_serv_args = [f"{RWR_SERV}", "verbose"]
+    # rwr_serv = subprocess.run(rwr_serv_args, cwd=RWR_ROOT.absolute(), encoding="utf-8")
+    rwr_serv = subprocess.Popen(rwr_serv_args, cwd=RWR_ROOT.absolute(), encoding="utf-8",
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    try:
+        # wait for the first prompt
+        wait_for_server_load(rwr_serv)
+        _consume_prompt(rwr_serv)
+        start_server_command = f"start_script {AS_SCRIPT.name} {path_to_package}"
+        print(f"Server loaded, sending '{start_server_command}'...")
+        # write the start script command to rwr server stdin
+        send_command(rwr_serv, start_server_command)
+        # wait again as the server now loads from overlays set in the script
+        wait_for_server_load(rwr_serv)
+        print(f"Package script start completed!")
+        # wait until Ctrl-C
+        while True:
+            # read a line from rwr server stdout
+            output_line = rwr_serv.stdout.readline().lstrip(">")
+            # strip the line for easier processing
+            stripped_line = output_line.strip()
+            print(stripped_line)
+            # time.sleep(10)
+            # todo: print status
+    except KeyboardInterrupt:
+        print("Ctrl-C detected, shutting down!")
+        rwr_serv.kill()
+
---
+++ scripts/start_vm_invasion_server.as
@@ -0,0 +1,74 @@
+// declare include paths
+#include "path://media/packages/vanilla/scripts"
+#include "path://media/packages/veteran-mode/scripts"
+
+#include "gamemode_invasion.as"
+// HACK: [VM] include trackers here for late addition to a derived GameModeInvasion metagame
+
+
+// --------------------------------------------
+void main(dictionary@ inputData) {
+    _setupLog("dev_verbose");
+    XmlElement inputSettings(inputData);
+    _log("VM debug: inputSettings = ");
+    _log(inputSettings.toStringWithFloats());
+    UserSettings settings;
+    // HACK: [VM] set the username from the local inputData as this main begins at campaign entry script
+    string username = "";
+    if (!inputData.get("username", username)) {
+        _log("username key not in inputData dict");
+    }
+    settings.m_username = username;
+    // 0 (greenbelts), 1 (graycollars), 2 (brownpants)
+    settings.m_factionChoice = 0;
+    settings.m_playerAiCompensationFactor = 1.0;
+    settings.m_enemyAiAccuracyFactor = 0.95;
+    settings.m_playerAiReduction = 0.0;
+    // HACK: mod for VM tester invasion
+    settings.m_teamKillPenaltyEnabled = false;
+    settings.m_completionVarianceEnabled = false;
+    settings.m_journalEnabled = false;
+    settings.m_fellowDisableEnemySpawnpointsSoldierCountOffset = 1;
+    // HACK: [VM] some of these VM things require XP and RP, improve reward factor and starting amounts
+    settings.m_xpFactor = 1.0;
+    /* settings.m_rpFactor = 1.0; */
+    settings.m_initialXp = 100.0;
+    settings.m_initialRp = 1000000.0;
+    // HACK: [VM] when there aren't enough enemies around, test these weapons on your allies instead XD
+    settings.m_friendlyFire = true;
+    // HACK: [VM] enable testing tools!
+    settings.m_testingToolsEnabled = true;
+    // configure veteran settings
+    settings.m_fov = true;
+
+    array<string> overlays = { "media/packages/invasion", "media/packages/veteran-mode" };
+    settings.m_overlayPaths = overlays;
+
+        settings.m_startServerCommand = """
+<command class='start_server'
+    server_name="Veteran Mode [TEST]"
+    server_port='1234'
+    comment='Coop campaign'
+    url=''
+    register_in_serverlist='1'
+    mode='COOP'
+    persistency='forever'
+    max_players='16'>
+    <client_faction id='0' />
+</command>
+""";
+
+    settings.print();
+    GameModeInvasion metagame(settings);
+    metagame.init();
+    // HACK: [VM] add local player as admin for easy testing, hacks, etc
+    if (!metagame.getAdminManager().isAdmin(metagame.getUserSettings().m_username)) {
+        metagame.getAdminManager().addAdmin(metagame.getUserSettings().m_username);
+    }
+    // HACK: [VM] late add VM trackers...
+
+    metagame.run();
+    metagame.uninit();
+
+    _log("ending execution");
+}
